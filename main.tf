fe65659e046351c4b1037a2ca9edb81a6d44ad06
smmih <s.m.mih@mail.ru>
2024-08-06 22:09:35 +0100

    the start conditions

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
-resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
+resource "aws_vpc" "develop" {
+  cidr_block       = var.default_cidr
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
 }
-resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
+
+resource "aws_subnet" "develop" {
+  tags = {
+    Name = var.vpc_name
+  }
+
+  vpc_id            = aws_vpc.develop.id
+  cidr_block        = var.default_cidr
+  availability_zone = var.availability_zone
+
 }
--- providers.tf
+++ providers.tf
@@ -1,15 +1,5 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = "~>1.8.4"
-}
-
-provider "yandex" {
-  token     = var.token
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.default_zone
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
